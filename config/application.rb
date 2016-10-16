ebf36ca0df2ad6f34824892cf9e6006afaa86abe
Shi-Ken Don <shiken.don@gmail.com>
2016-10-16 18:22:23 +0800

    Use structure.sql instead of schema.rb

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Denpaio
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Use structure.sql instead of schema.rb
+    config.active_record.schema_format = :sql
   end
 end
---
+++ db/structure.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+CREATE TABLE "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO schema_migrations (version) VALUES ;
+
+
