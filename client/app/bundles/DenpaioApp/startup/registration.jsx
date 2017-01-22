a73c6c8e1eaa8f9c27c99f4d640701c20a2606dd
Shi-Ken Don <shiken.don@gmail.com>
2017-01-23 05:34:06 +0800

    Rename HelloWorld component to DenpaioApp

---
+++ client/app/bundles/DenpaioApp/startup/registration.jsx
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import DenpaioApp from '../components/DenpaioApp';
+
+// This is how react_on_rails can see the DenpaioApp in the browser.
+ReactOnRails.register({
+  DenpaioApp,
+});
