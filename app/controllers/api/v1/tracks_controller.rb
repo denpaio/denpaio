df48046aefd3fdbeffb686be1733d2e2016d19a3
Shi-Ken Don <shiken.don@gmail.com>
2017-01-31 05:22:59 +0800

    Generate tracks API v1 controller

---
+++ app/controllers/api/v1/tracks_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::TracksController < ApplicationController
+  respond_to :xml, :json
+
+  def search
+    itunes = ITunes::Client.new
+    songs = itunes.music(params[:q], country: :jp)
+    @object = songs
+    respond_with :api, :v1, @object
+  rescue => e
+    @object = {
+      error_message: e.message,
+      cause: e.cause
+    }
+    respond_with :api, :v1, @object, status: 422
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :tracks do
+        get :search, on: :collection
+      end
+    end
+  end
 end
