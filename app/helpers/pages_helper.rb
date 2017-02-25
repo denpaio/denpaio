3ebbe1a46ccbfd748a23631806bc1ed9c530097d
Shi-Ken Don <shiken.don@gmail.com>
2017-02-26 01:36:09 +0800

    Fix default background image url

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,7 +4,7 @@ module PagesHelper
     react_component(
       'DenpaioApp',
       props: {
-        default_background_image: image_url('pixiv_46722568_q70')
+        default_background_image: image_url('pixiv_46722568_q70.jpg')
       },
       prerender: false
     )
