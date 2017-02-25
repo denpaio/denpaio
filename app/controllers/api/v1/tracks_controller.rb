04abf08ed246b35cf5b308796d12db751b947928
Shi-Ken Don <shiken.don@gmail.com>
2017-02-26 01:06:56 +0800

    Add missing frozen string literal comment

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Api::V1::TracksController < ApplicationController
   respond_to :xml, :json
 
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module PagesHelper
   def render_denpaio_app
     react_component(
