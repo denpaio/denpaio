70599283cf221a13a165966e648f898a2f5fffd2
Shi-Ken Don <shiken.don@gmail.com>
2017-02-26 18:53:23 +0800

    Add encrypt and decrypt method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  def encrypt_by_secret_key(value)
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base)
+    crypt.encrypt_and_sign(value)
+  end
+
+  def decrypt_by_secret_key(value)
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base)
+    crypt.decrypt_and_verify(value)
+  end
 end
