b1cfc1d80fc89363dcd1a36e1f40473b07db39ae
Shi-Ken Don <shiken.don@gmail.com>
2017-02-26 18:55:15 +0800

    Add proxy to all insecure urls of iTunes response

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::TracksController < ApplicationController
   def search
     itunes = ITunes::Client.new
     songs = itunes.music(params[:q], country: :jp)
-    @object = songs
+    @object = proxy_all_insecure_urls(songs)
     respond_with :api, :v1, @object
   rescue => e
     @object = {
@@ -13,5 +13,18 @@ class Api::V1::TracksController < ApplicationController
       cause: e.cause
     }
     respond_with :api, :v1, @object, status: 422
+  end
+
+  def proxy_all_insecure_urls(songs)
+    songs.tap do |s|
+      s.results.to_a.each do |result|
+        %w(artwork_url30 artwork_url60 artwork_url100 preview_url).each do |attr|
+          url = result[attr]
+          next if url.start_with?('https://')
+          encrypted_url = encrypt_by_secret_key(url)
+          result[attr] = url_for(controller: '/proxies', action: :any, url: encrypted_url)
+        end
+      end
+    end
   end
 end
