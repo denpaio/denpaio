edde2c5e8f3e7723fca70dd3b6514b49fd863729
Shi-Ken Don <shiken.don@gmail.com>
2017-04-29 15:33:00 +0800

    Fix iTunes search scope on musics

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::TracksController < ApplicationController
 
   def search
     itunes = ITunes::Client.new
-    songs = itunes.music(params[:q], country: :jp)
+    songs = itunes.music(params[:q], country: :jp, entity: :song, limit: 25)
     @object = proxy_all_insecure_urls(songs)
     respond_with :api, :v1, @object
   rescue => e
@@ -20,6 +20,7 @@ class Api::V1::TracksController < ApplicationController
       s.results.to_a.each do |result|
         %w(artwork_url30 artwork_url60 artwork_url100 preview_url).each do |attr|
           url = result[attr]
+          next if url.nil?
           next if url.start_with?('https://')
           encrypted_url = encrypt_by_secret_key(url)
           result[attr] = url_for(controller: '/proxies', action: :any, url: encrypted_url)
