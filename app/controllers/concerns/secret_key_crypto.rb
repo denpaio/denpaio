f850fe804cf25af78f875a0d4d32b0c13c1f154d
Shi-Ken Don <shiken.don@gmail.com>
2017-05-01 00:30:15 +0800

    Refactor secret_key crypto methods

---
+++ app/controllers/concerns/secret_key_crypto.rb
@@ -0,0 +1,17 @@
+require 'active_support/concern'
+
+module SecretKeyCrypto
+  extend ActiveSupport::Concern
+
+  included do
+    def encrypt_by_secret_key(value)
+      crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base)
+      crypt.encrypt_and_sign(value)
+    end
+
+    def decrypt_by_secret_key(value)
+      crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base)
+      crypt.decrypt_and_verify(value)
+    end
+  end
+end
