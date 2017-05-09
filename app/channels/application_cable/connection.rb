cb42cec239d28e3b7603435caa5aaf3f2fc67484
Shi-Ken Don <shiken.don@gmail.com>
2017-05-10 03:26:02 +0800

    Fix channels broadcasting using sessions

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_session
+
+    def connect
+      self.current_session = session
+    end
+
+    def session
+      cookies.encrypted[Rails.application.config.session_options[:key]]
+    end
   end
 end
--- app/channels/playlist_channel.rb
+++ app/channels/playlist_channel.rb
@@ -1,13 +1,14 @@
 class PlaylistChannel < ApplicationCable::Channel
   def subscribed
     stream_from 'playlist'
+    stream_for current_session
   end
 
   def reload
     @last_play = Play.where.not(played_at: nil).first
     @plays = Play.where(played_at: nil).or(Play.where(id: @last_play.id)).first(10)
 
-    ActionCable.server.broadcast('playlist', {
+    PlaylistChannel.broadcast_to(current_session, {
       action: 'reload',
       objects: @plays.as_json(include: :track)
     })
