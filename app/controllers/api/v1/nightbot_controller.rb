b6d3f2ee6b800106d8cc867aa164d6fe0c165915
Shi-Ken Don <shiken.don@gmail.com>
2017-05-27 18:13:29 +0800

    Add playlist and request support to Nightbot API

--- app/controllers/api/v1/nightbot_controller.rb
+++ app/controllers/api/v1/nightbot_controller.rb
@@ -7,8 +7,19 @@ class Api::V1::NightbotController < ApplicationController
     when 'current'
       track = Play.where.not(played_at: nil).first.track
       render text: format('Now playing: %s - %s', track.artist, track.name)
-    when 'playlist', 'request'
-      render text: 'TODO'
+    when 'playlist'
+      plays = Play.where(played_at: nil)
+      render text: plays.map { |play| play.track.name }.reverse.join(', ')
+    when /request\s+(?<name>.+)/
+      name = Regexp.last_match(:name)
+      track = Track.find_by(name: name)
+
+      if track
+        track.plays.create!
+        render text: format('Track name `%s\' successful requested', name)
+      else
+        render text: format('Track name `%s\' not found', name)
+      end
     else
       render text: 'Usage: !songs current|playlist|request'
     end
