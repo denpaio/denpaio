68e5b402143d791cbf05eb6ee532ef176ecc745a
Shi-Ken Don <shiken.don@gmail.com>
2017-06-23 10:29:32 +0800

    Fix reload response of playlist channel

--- app/channels/playlist_channel.rb
+++ app/channels/playlist_channel.rb
@@ -8,7 +8,7 @@ class PlaylistChannel < ApplicationCable::Channel
 
   def reload
     @last_play = Play.where.not(played_at: nil).first
-    @plays = Play.where(played_at: nil).or(Play.where(id: @last_play.id)).first(10)
+    @plays = Play.not_played.or(Play.where(id: @last_play.id))
 
     PlaylistChannel.broadcast_to(
       current_session,
