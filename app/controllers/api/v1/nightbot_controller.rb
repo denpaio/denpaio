1053e6d65d76bd0eb3fa60a04e0683cddffff488
Shi-Ken Don <shiken.don@gmail.com>
2018-01-13 03:01:50 +0800

    Fix nightbot controller rendering for Rails 5.1.0

--- app/controllers/api/v1/nightbot_controller.rb
+++ app/controllers/api/v1/nightbot_controller.rb
@@ -7,22 +7,22 @@ class Api::V1::NightbotController < ApplicationController
     case params[:q]
     when 'current'
       track = Play.where.not(played_at: nil).first.track
-      render text: format('Now playing: %s - %s', track.artist, track.name)
+      render plain: format('Now playing: %s - %s', track.artist, track.name)
     when 'playlist'
       plays = Play.where(played_at: nil)
-      render text: plays.map { |play| play.track.name }.reverse.inspect
+      render plain: plays.map { |play| play.track.name }.reverse.inspect
     when /request\s+(?<name>.+)/
       name = Regexp.last_match(:name)
       track = Track.find_by(name: name)
 
       if track
         track.plays.create!
-        render text: format('Track name `%s\' successful requested', name)
+        render plain: format('Track name `%s\' successful requested', name)
       else
-        render text: format('Track name `%s\' not found', name)
+        render plain: format('Track name `%s\' not found', name)
       end
     else
-      render text: 'Usage: !songs current|playlist|request'
+      render plain: 'Usage: !songs current|playlist|request'
     end
   end
 end
